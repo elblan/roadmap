478ce7007a8029b8e10c43fa1b028550593f64bd
Etienne <etienneleblan@gmail.com>
2020-12-08 23:25:59 +0100

    latest

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,11 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item">
-        <router-link to="/">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-        /></router-link>
-      </a>
+      <router-link to="/" class="navbar-item">
+        <img
+          style="object-fit: contain;"
+          src="https://firebasestorage.googleapis.com/v0/b/roadmapfree.appspot.com/o/roadmap%20(7).png?alt=media&token=b9543a6d-b228-47ce-81c8-f0ad7f5aa46e"
+      /></router-link>
     </div>
 
     <div class="navbar-menu">
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/roadmap/" : "/"
-};
+  publicPath: process.env.NODE_ENV === 'production' ? '/roadmap/' : '/'
+}
